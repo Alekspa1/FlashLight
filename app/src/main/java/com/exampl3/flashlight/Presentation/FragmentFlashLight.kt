0ecbfd867a15937f12f32c727fed10fb6d7cede8
Alex <apereverzev47@gmail.com>
2023-12-23 14:08:25 +0400

    Релиз

--- app/src/main/java/com/exampl3/flashlight/Presentation/FragmentFlashLight.kt
+++ app/src/main/java/com/exampl3/flashlight/Presentation/FragmentFlashLight.kt
@@ -1,19 +1,15 @@
 package com.exampl3.flashlight.Presentation
 
-import android.content.Context
-import android.hardware.camera2.CameraManager
-import android.os.Build
+
 import android.os.Bundle
-import android.os.VibrationEffect
-import android.os.VibratorManager
-import android.util.Log
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.annotation.RequiresApi
+
+
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModel
+
 import com.exampl3.flashlight.R
 import com.exampl3.flashlight.databinding.FragmentBlankFlashLightBinding
 
@@ -33,7 +29,6 @@ class FragmentFlashLight : Fragment() {
     }
 
 
-    @RequiresApi(Build.VERSION_CODES.S)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         model = ViewModelFlashLight()
