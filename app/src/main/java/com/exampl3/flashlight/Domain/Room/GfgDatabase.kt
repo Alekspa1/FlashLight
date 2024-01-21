eea585993806864757c568ca117478ccf4470c26
Alex <apereverzev47@gmail.com>
2024-01-21 11:51:09 +0400

    Сделал миграция БД

--- app/src/main/java/com/exampl3/flashlight/Domain/Room/GfgDatabase.kt
+++ app/src/main/java/com/exampl3/flashlight/Domain/Room/GfgDatabase.kt
@@ -7,7 +7,10 @@ import androidx.room.RoomDatabase
 
 @Database(entities = [Item::class],
     version = 3,
-    autoMigrations = [AutoMigration (from = 2, to = 3)],
+    autoMigrations = [
+        AutoMigration (from = 1, to = 2),
+        AutoMigration (from = 2, to = 3)
+    ]
 )
 abstract class GfgDatabase : RoomDatabase()
 {
