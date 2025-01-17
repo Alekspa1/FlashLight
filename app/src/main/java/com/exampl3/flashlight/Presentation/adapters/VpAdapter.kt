d2d7b0e9470ad5bc7d94988f6de78ffb57361861
DRAGON <apereverzev47@gmail.com>
2025-01-17 15:39:41 +0300

    Изменил названия фрагмента календаря

--- app/src/main/java/com/exampl3/flashlight/Presentation/adapters/VpAdapter.kt
+++ app/src/main/java/com/exampl3/flashlight/Presentation/adapters/VpAdapter.kt
@@ -3,14 +3,14 @@ package com.exampl3.flashlight.Presentation.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.exampl3.flashlight.Presentation.FragmentFlashLight
+import com.exampl3.flashlight.Presentation.FragmentCalendar
 import com.exampl3.flashlight.Presentation.FragmentList
 import com.exampl3.flashlight.Presentation.FragmentNotebook
 
 private val listFrag = listOf(
     FragmentNotebook.newInstance(),
     FragmentList.newInstance(),
-    FragmentFlashLight.newInstance()
+    FragmentCalendar.newInstance()
 )
 
 class VpAdapter(fragment: FragmentActivity) : FragmentStateAdapter(fragment) {
