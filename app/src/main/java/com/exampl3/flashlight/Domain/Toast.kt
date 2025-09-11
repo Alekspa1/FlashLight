2096b0e7fbdfd22e651ef243ecb8b708265f2dc3
DRAGON <apereverzev47@gmail.com>
2025-09-11 15:33:29 +0300

    Релиз

--- app/src/main/java/com/exampl3/flashlight/Domain/Toast.kt
+++ app/src/main/java/com/exampl3/flashlight/Domain/Toast.kt
@@ -3,6 +3,6 @@ package com.exampl3.flashlight.Domain
 import android.content.Context
 import android.widget.Toast
 
-fun toastFun(contex: Context, value: String){
+fun ToastFun(contex: Context, value: String){
     Toast.makeText(contex, value, Toast.LENGTH_SHORT).show()
 }
