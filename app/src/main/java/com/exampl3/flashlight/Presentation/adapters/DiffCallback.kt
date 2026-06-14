124010941ad15fe4a9fec152b0de566e0498e3bc
Alekspa1 <141168649+Alekspa1@users.noreply.github.com>
2026-06-14 18:56:09 +0300

    Update DiffCallback.kt

--- app/src/main/java/com/exampl3/flashlight/Presentation/adapters/DiffCallback.kt
+++ app/src/main/java/com/exampl3/flashlight/Presentation/adapters/DiffCallback.kt
@@ -5,19 +5,13 @@ import com.exampl3.flashlight.Data.Room.Item
 
 class DiffCallback : DiffUtil.ItemCallback<Item>() {
     override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
-        return oldItem.id == newItem.id // Сравниваем по ID
+        return oldItem.id == newItem.id // Сравниваем строго по ID
     }
 
     override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
-        return oldItem == newItem // Проверяем все поля, включая sort
-    }
-
-    // ДОБАВЬТЕ ЭТОТ МЕТОД:
-    override fun getChangePayload(oldItem: Item, newItem: Item): Any? {
-        // Если старый и новый элемент отличаются ТОЛЬКО полем sort (текст, ID и будильники те же)
-        if (oldItem.copy(sort = newItem.sort) == newItem) {
-            return true // Выбрасываем флаг-сигнал: "Изменился только порядок!"
-        }
-        return super.getChangePayload(oldItem, newItem)
+        // Жесткое сравнение ВСЕХ полей, включая sort.
+        // Теперь DiffUtil сразу заметит, что sort изменился, и принудительно 
+        // заставит ячейку обновить свой текст через bind()!
+        return oldItem == newItem 
     }
 }
