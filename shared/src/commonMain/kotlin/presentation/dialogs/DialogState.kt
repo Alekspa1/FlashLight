8c919391f79a9907aa7840229ed4da842cc81c97
Alekspa1 <141168649+Alekspa1@users.noreply.github.com>
2026-07-02 16:33:10 +0300

    Update DialogState.kt

--- shared/src/commonMain/kotlin/presentation/dialogs/DialogState.kt
+++ shared/src/commonMain/kotlin/presentation/dialogs/DialogState.kt
@@ -3,7 +3,6 @@ package presentation.dialogs
 import data.room.Item
 
 data class DialogState(
-    val isActive: Boolean = false,
     val isWho: String = CommonConst.DEFAULT_DIALOG,
     val item: Item? = null
 )
