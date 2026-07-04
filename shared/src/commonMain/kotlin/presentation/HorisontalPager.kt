ba31ee62de609b3c86fb23241ed668613af84ee7
Alekspa1 <141168649+Alekspa1@users.noreply.github.com>
2026-07-04 23:11:29 +0300

    Update HorisontalPager.kt

--- shared/src/commonMain/kotlin/presentation/HorisontalPager.kt
+++ shared/src/commonMain/kotlin/presentation/HorisontalPager.kt
@@ -236,7 +236,7 @@ fun MainWeatherPager(paddingValues: PaddingValues = PaddingValues(),
         ) { pageIndex ->
             when (pageIndex) {
                 0 -> {
-                    Notebook(viewModel,pageIndex)
+                    Notebook(viewModel,pagerState)
                 }
                 1 -> {
                     ListToDo(list = todoList,
