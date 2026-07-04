383955a12060bf67cad211399dcb8a6316a3c469
Alekspa1 <141168649+Alekspa1@users.noreply.github.com>
2026-07-04 19:38:59 +0300

    Update build.gradle.kts

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -54,10 +54,12 @@ kotlin {
             implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.8.0")
 
             // Обновляем оба модуля FileKit до актуальной версии 0.14.2
-            implementation("io.github.vinceglb:filekit-compose:0.14.2")
+              // Зависимости FileKit (0.14.2)
+            implementation("io.github.vinceglb:filekit-core:0.14.2")
+            implementation("io.github.vinceglb:filekit-dialogs-compose:0.14.2")
             implementation("io.github.vinceglb:filekit-coil:0.14.2")
 
-
+            // Зависимости Coil (3.5.0)
             implementation("io.coil-kt.coil3:coil-compose:3.5.0")
    
         }
