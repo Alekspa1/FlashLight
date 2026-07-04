de49e0e1102f712f441e34962be57b16887e15de
Alekspa1 <141168649+Alekspa1@users.noreply.github.com>
2026-07-04 19:17:35 +0300

    Update build.gradle.kts

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -54,6 +54,7 @@ kotlin {
             implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.8.0")
 
             implementation("io.github.vinceglb:filekit-compose:0.8.8")
+             implementation("io.github.vinceglb:filekit-coil:0.8.0") 
 // Поменяли 3.5.0 на стабильную 3.0.4
             implementation("io.coil-kt.coil3:coil-compose:3.3.0")
             implementation("io.coil-kt.coil3:coil-network-ktor3:3.3.0")
