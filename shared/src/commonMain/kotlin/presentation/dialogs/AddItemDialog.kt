508aea33a79d3b334328c2579a9f5b60c3360a8e
Alekspa1 <141168649+Alekspa1@users.noreply.github.com>
2026-07-06 12:24:54 +0300

    Update AddItemDialog.kt

--- shared/src/commonMain/kotlin/presentation/dialogs/AddItemDialog.kt
+++ shared/src/commonMain/kotlin/presentation/dialogs/AddItemDialog.kt
@@ -224,7 +224,7 @@ fun OpenImage(uri: String,onDismiss : () -> Unit){
                     modifier = Modifier
                         .fillMaxWidth() // Растягивается по ширине
                         .wrapContentHeight(), // Высота подстроится автоматически и корректно
-                     .clip(RoundedCornerShape(12.dp))
+                     .clip(RoundedCornerShape(12.dp)),
                     contentScale = ContentScale.Fit // Картинка гарантированно поместится целиком без обрезки
                 )
             }
