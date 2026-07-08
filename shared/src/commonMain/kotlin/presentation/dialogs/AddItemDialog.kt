aedf7e0ab113063470235082bab6ff205ff0ff7b
Alekspa1 <141168649+Alekspa1@users.noreply.github.com>
2026-07-08 18:38:56 +0300

    Update AddItemDialog.kt

--- shared/src/commonMain/kotlin/presentation/dialogs/AddItemDialog.kt
+++ shared/src/commonMain/kotlin/presentation/dialogs/AddItemDialog.kt
@@ -57,6 +57,7 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.background
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
