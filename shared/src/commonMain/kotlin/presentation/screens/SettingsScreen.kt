17623e6575616cd8e2e17029303fabfe8006eacf
Alekspa1 <141168649+Alekspa1@users.noreply.github.com>
2026-07-13 17:05:38 +0300

    Create SettingsScreen.kt

---
+++ shared/src/commonMain/kotlin/presentation/screens/SettingsScreen.kt
@@ -0,0 +1,12 @@
+package presentation.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@Compasable
+fun SettingsScreen(onBack : () -> Untit = {){
+
+
+  Text(text = "Экран настроек", color = Color.White)
+}
