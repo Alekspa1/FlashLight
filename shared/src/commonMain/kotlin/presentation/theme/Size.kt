7adb5dd81504cef389e9450dbe20c60803eb8498
Alekspa1 <141168649+Alekspa1@users.noreply.github.com>
2026-07-20 11:37:07 +0300

    Update Size.kt

--- shared/src/commonMain/kotlin/presentation/theme/Size.kt
+++ shared/src/commonMain/kotlin/presentation/theme/Size.kt
@@ -16,6 +16,18 @@ sealed interface Size {
 }
 
 
+
+data class SizeSmall(
+    override val textMenu: TextUnit = 20.sp,
+    override val textItem: TextUnit = 15.sp,
+    override val lineHeightItem: TextUnit = 17.sp, // Уменьшили на 3
+    override val drawerBottomMenuText: TextUnit = 11.sp,
+    override val textNoteBook: TextUnit = 14.sp,
+    override val textDesc: TextUnit = 9.sp,
+    override val textAlarm: TextUnit = 9.sp,
+    override val lineHeightDescAndAlarm: TextUnit = 11.sp // Уменьшили на 3
+) : Size
+
 data class SizeNormal(
     override val textMenu: TextUnit = 23.sp,
     override val textItem: TextUnit = 18.sp,
@@ -25,5 +37,16 @@ data class SizeNormal(
     override val textDesc: TextUnit = 12.sp,
     override val textAlarm: TextUnit = 12.sp,
     override val lineHeightDescAndAlarm: TextUnit = 14.sp
+) : Size
 
+
+data class SizeLarge(
+    override val textMenu: TextUnit = 26.sp,
+    override val textItem: TextUnit = 21.sp,
+    override val lineHeightItem: TextUnit = 23.sp, // Увеличили на 3
+    override val drawerBottomMenuText: TextUnit = 17.sp,
+    override val textNoteBook: TextUnit = 20.sp,
+    override val textDesc: TextUnit = 15.sp,
+    override val textAlarm: TextUnit = 15.sp,
+    override val lineHeightDescAndAlarm: TextUnit = 17.sp // Увеличили на 3
 ) : Size
