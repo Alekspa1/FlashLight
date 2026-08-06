d602bc54085e351eb9474230faa33eefac831820
Alekspa1 <141168649+Alekspa1@users.noreply.github.com>
2026-08-06 18:32:19 +0300

    Update PremiumScreen.kt

--- shared/src/commonMain/kotlin/presentation/screens/PremiumScreen.kt
+++ shared/src/commonMain/kotlin/presentation/screens/PremiumScreen.kt
@@ -51,6 +51,9 @@ import presentation.theme.Size
 import presentation.theme.SizeNormal
 import presentation.theme.Theme
 import presentation.theme.ThemeNeon
+import androidx.compose.foundation.Image
+import androidx.compose.ui.layout.ContentScale
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun PremiumScreen(
