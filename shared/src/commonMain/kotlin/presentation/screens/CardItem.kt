fe73aebb481b45f172dda5b3eb938ca03c04d41d
Alekspa1 <141168649+Alekspa1@users.noreply.github.com>
2026-08-08 17:27:00 +0300

    Update CardItem.kt

--- shared/src/commonMain/kotlin/presentation/screens/CardItem.kt
+++ shared/src/commonMain/kotlin/presentation/screens/CardItem.kt
@@ -91,7 +91,7 @@ fun CardItem(
     theme: Theme = ThemeNeon(),
     size: Size = SizeNormal(),
     dragModifier: Modifier = Modifier,
-    onClick: (Item, Int, SubItem?) -> Unit = { _, _, _ -> },
+    onClick: (Item, Int) -> Unit = { _, _, _ -> },
     onSubDragDropped: (List<SubItem>) -> Unit = {} // Колбэк для отправки нового порядка во ViewModel
 ) {
     var isExpanded by remember { mutableStateOf(false) }
