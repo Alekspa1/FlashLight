340704d9dbdd3ae81b5821ba02f766e92ed3edf2
Alekspa1 <141168649+Alekspa1@users.noreply.github.com>
2026-08-08 15:54:48 +0300

    Update ListToDo.kt

--- shared/src/commonMain/kotlin/presentation/screens/ListToDo.kt
+++ shared/src/commonMain/kotlin/presentation/screens/ListToDo.kt
@@ -31,6 +31,7 @@ import sh.calvin.reorderable.rememberReorderableLazyListState
 
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.runtime.LaunchedEffect
 
 @Composable
 fun ListToDo(
