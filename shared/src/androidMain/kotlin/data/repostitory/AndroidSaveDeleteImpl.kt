5125b1f8ecd1ec195d1f0fc986e1484e6412a856
DRAGON <apereverzev47@gmail.com>
2026-08-22 09:55:22 +0300

    контрольный

--- shared/src/androidMain/kotlin/data/repostitory/AndroidSaveDeleteImpl.kt
+++ shared/src/androidMain/kotlin/data/repostitory/AndroidSaveDeleteImpl.kt
@@ -34,8 +34,8 @@ class AndroidSaveDeleteImpl(private val context: Context) : SaveDeleteImageRepos
         try {
             if (fileName.isEmpty()) return
             // Находим файл по имени в папке images и удаляем его
-            val fullPath = getUri(fileName) 
-        val file = File(fullPath)
+            val fullPath = getUri(fileName)
+            val file = File(fullPath)
             if (file.exists()) {
                 file.delete()
             }
