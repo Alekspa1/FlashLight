d0b7a011252c10682a1f6d7252f500d5c53e0f7f
Alekspa1 <141168649+Alekspa1@users.noreply.github.com>
2026-08-24 15:38:23 +0300

    Update Theme.kt

--- shared/src/commonMain/kotlin/presentation/theme/Theme.kt
+++ shared/src/commonMain/kotlin/presentation/theme/Theme.kt
@@ -16,7 +16,7 @@ import androidx.compose.material.icons.filled.Update
 import androidx.compose.material.icons.filled.Upgrade
 import androidx.compose.material.icons.filled.WorkspacePremium
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color*
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
